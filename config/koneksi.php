2155c592261024e288af749a4b09303575062c38
raihankusdinar <161433859+raihankusdinar@users.noreply.github.com>
2024-02-28 13:27:33 +0700

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$hostname = "localhost";
+$userdb = "root";
+$passdb = "";
+$namedb = "albumku";
+
+$conn = mysqli_connect($hostname, $userdb, $passdb, $namedb);
+
+// if ($conn) {
+//     echo "Terhubung";
+// } else {
+//     echo "tidak terhubung";
+// }
+?>
